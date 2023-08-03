a3ae11209a2666c8fa89bd7eceb2e64ac5375291
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-03 17:30:13 +0330

    model userState and show username

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,4 +1,12 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Zana</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 export default UserName;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import store from "./store.js";
 
 import App from "./App.jsx";
 
@@ -7,6 +10,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
