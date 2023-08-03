287dddbf9577b1ced6b875ba8112eb3a2923f53f
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-03 08:37:48 +0330

    apply flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,4 +1,4 @@
 function UserName() {
-  return <div className="hidden text-sm font-semibold">Zana</div>;
+  return <div className="hidden text-sm font-semibold md:block">Zana</div>;
 }
 export default UserName;
