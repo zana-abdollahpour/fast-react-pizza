6daa15ccbab07c8d966974428d5ecbc7ee53185e
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-05 16:34:40 +0330

    fetch ingredient data without navigation

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,4 +1,5 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect } from "react";
+import { useFetcher, useLoaderData } from "react-router-dom";
 
 import { getOrder } from "../../services/apiRestaurant";
 import {
@@ -11,6 +12,14 @@ import OrderItem from "./OrderItem";
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+    },
+    [fetcher],
+  );
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -54,7 +63,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-y">
         {cart.map((item) => (
-          <OrderItem key={item.pizzaId} item={item} />
+          <OrderItem
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            item={item}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
