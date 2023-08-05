6400b51034ccf653daee250fb834186dc8896ef7
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-06 00:30:50 +0330

    update cart data (priority)

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CreateOrder, {
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,8 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../utils/helpers";
 
 import OrderItem from "./OrderItem";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -88,6 +89,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
